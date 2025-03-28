35f7b7dd16e005ec13d53ead1bbda9500dabac07
Victor <victor.kihiko@student.moringaschool.com>
2025-03-29 01:29:51 +0300

    added set trace ipdb shell

--- lib/kickstarter_scraper.py
+++ lib/kickstarter_scraper.py
@@ -13,6 +13,7 @@ def create_project_dict():
     with open('./fixtures/kickstarter.html') as file:
         html = file.read()
     kickstarter = BeautifulSoup(html, 'html.parser')
+    ipdb.set_trace()
     projects = {}
     # Iterate through the projects
     for project in kickstarter.select("li.project.grid_4"):
